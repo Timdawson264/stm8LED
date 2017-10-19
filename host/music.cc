4ed5dbad004bb4729885a85a74cd8ec169a8fe28
Tim Dawson <tim.dawson@nyriad.com>
2017-10-19 19:56:32 +1300

    Added continuous color changing.

--- host/music.cc
+++ host/music.cc
@@ -86,6 +86,8 @@ struct context
     pixel pix;
     int pix_fd;
     float all_time_max;
+    float h_offset; //Running offset of colour - just to change the colour up
+    float h_offset_step;
 };
 
 
@@ -124,24 +126,29 @@ int record( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
     //DO FFT
     fftwf_execute( ctx->plan );
 
-
     for(size_t i=0; i<fft_half; i++)
     {
         ctx->out_buffer[i][0] *= 2.0/fft_size;
         ctx->out_buffer[i][1] *= 2.0/fft_size;
         ctx->processed[i] = powf(ctx->out_buffer[i][0], 2) + powf(ctx->out_buffer[i][1], 2); // C^2 = A^2 + B^2
-        ctx->processed[i] = sqrt( ctx->processed[i] ) ; // C = sqrt( C^2 )
+        ctx->processed[i] = sqrt( std::abs( ctx->processed[i] ) ) ; // C = sqrt( C^2 )
     }
 
-    float* max_e = std::max_element( ctx->processed+1, ctx->processed+fft_half );
+    float* max_e = std::max_element( ctx->processed, ctx->processed+fft_half );
+
+    //Track the Dynamic range
     ctx->all_time_max = std::max( ctx->all_time_max, *max_e ); //max this the new max if it wins
 
-    *max_e = std::min( *max_e/ctx->all_time_max  , 1.0f );
+    //Normalize
+    *max_e = *max_e/ctx->all_time_max;
 
     size_t max_idx = ( uintptr_t(max_e) - uintptr_t(ctx->processed) ) / sizeof(float);
 
 
-    float h = ( (float)max_idx/fft_half ) * 360; //degrees 0-360
+    //TODO: Maybe a running average would look better - stop jaring colour changes
+    float h = ( (float)max_idx/fft_half ) * 360.0; //degrees 0-360
+    h = fmod( h+ctx->h_offset, 360.0 ); //offset H so the colour is always different
+    ctx->h_offset = fmod( ctx->h_offset + ctx->h_offset_step, 360.0 );  //slowly change the colour over time.
 
     std::cout << "MAX_n:\t" << max_idx << "\tMAX_v:\t" << *max_e << "\tH:\t" << h << std::endl;
 
@@ -190,17 +197,21 @@ int main( int argc, char** argv )
     parameters.nChannels = 1;
     parameters.firstChannel = 0;
     unsigned int sampleRate = 8000;
-    unsigned int bufferFrames = (1./60.) * sampleRate;
+    size_t update_rate = 60; //LED update rate
+    unsigned int bufferFrames = sampleRate / update_rate;
 
-    std::cerr << "FFT SIZE: " << bufferFrames*2 << std::endl;
+    //Rotate the colour wheel every 5 minutes
+    ctx.h_offset_step = 360.0f / (float)(update_rate * 60 * 5 );
 
-    ctx.n = 1024;
+
+    ctx.n = 512 ;
     ctx.in_buffer = (float*) malloc( sizeof(float) * ctx.n );
     ctx.out_buffer = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * ctx.n);
     ctx.processed = (float*) malloc( sizeof(float) * ctx.n / 2 );
 
     ctx.plan = fftwf_plan_dft_r2c_1d( ctx.n, ctx.in_buffer, ctx.out_buffer, FFTW_MEASURE );
 
+    std::cerr << "FFT SIZE: " << ctx.n << std::endl;
     try {
         adc.openStream( NULL, &parameters, RTAUDIO_FLOAT32,
                         sampleRate, &bufferFrames, &record, &ctx );
